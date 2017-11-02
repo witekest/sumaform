762d176e425691f02175637d42c07ad01095a4f9
Silvio Moioli <smoioli@suse.de>
2017-11-02 10:43:22 +0100

    AWS: change configure_aws_tunnels.rb to run automatically

--- configure_aws_tunnels.rb
+++ configure_aws_tunnels.rb
@@ -1,38 +1,41 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
-require 'json'
+require 'optparse'
 
-output = JSON.parse(`terraform output -json`)
+key_file = ""
+mirror_public_name = ""
+single_instances = []
+multiple_instances = []
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
 
-mirror_private_name = output["mirror_public_name"]["value"]
-
-single_instances = output
-  .map do |name, value|
-    if name =~ /aws_(.*)_private_name$/
-      { symbolic_name: $1, private_name: value["value"] }
-    end
+  opts.on("-k", "--key-file [KEY_FILE]", "key file path") do |ext|
+    key_file = ext
   end
-  .compact
 
-multiple_instances = output
-  .map do |name, value|
-    if name =~ /aws_(.*)_private_names$/
-      value["value"].each_with_index.map do |name, index|
-        { symbolic_name: "#{$1}-#{index}", private_name: name }
-      end
-    end
+  opts.on("-m", "--mirror [MIRROR_PUBLIC_NAME]", "mirror's public name") do |ext|
+    mirror_public_name = ext
   end
-  .compact
-  .flatten
+
+  opts.on("-s", "--single-instance [SYMBOLIC_NAME:PRIVATE_NAME]", "symbolic name to private AWS name association for a single instance") do |ext|
+    single_instances << { symbolic_name: ext.split(":")[0], private_name: ext.split(":")[1] }
+  end
+
+  opts.on("-p", "--multiple-instance [SYMBOLIC_NAME:PRIVATE_NAME,PRIVATE_NAME,...]", "symbolic name to private AWS name association for multiple instances") do |ext|
+    base_symbolic_name = ext.split(":")[0]
+    multiple_instances += ext.split(":")[1].split(",").each_with_index.map do |name, index|
+      { symbolic_name: "#{base_symbolic_name}-#{index}", private_name: name }
+    end.flatten
+  end
+end.parse!
 
 instances = single_instances + multiple_instances
-key_file = output["key_file"]["value"]
 
 tunnel_string = <<-eos
 # sumaform configuration start
 Host mirror
-  HostName #{mirror_private_name}
+  HostName #{mirror_public_name}
   StrictHostKeyChecking no
   User root
   IdentityFile #{key_file}
@@ -54,7 +57,7 @@ instances.each do |instance|
     tunnel_string += "    LocalForward 8043 127.0.0.1:443\n"
   end
   if instance[:symbolic_name] =~ /grafana/
-    tunnel_string += "    LocalForward 8080 127.0.0.1:8080\n"
+    tunnel_string += "    LocalForward 8080 127.0.0.1:80\n"
     tunnel_string += "    LocalForward 9090 127.0.0.1:9090\n"
   end
 end
