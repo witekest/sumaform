29721ac27598797163de4331ccaaa2af35f45937
Matei Albu <malbu@suse.de>
2019-03-15 18:25:12 +0100

    debug default.minimal

--- salt/first_deployment_highstate.sh
+++ salt/first_deployment_highstate.sh
@@ -3,13 +3,12 @@
 # Applies the "minimal" state in isolation to set the hostname and update Salt itself
 # then applies the highstate
 
-if [ -f /etc/debian_version ]; then
-   echo "Debian detected. exiting"
-   exit 1;
-fi
 
+if [ -f /etc/debian_version ]; then
+salt-call --local --file-root=/root/salt/ --log-level=debug state.sls default.minimal ||:
+else
 salt-call --local --file-root=/root/salt/ --log-level=quiet --output=quiet state.sls default.minimal ||:
-echo "---------------------------------------------------------------------------------------------------"
+fi
 salt-call --local --file-root=/root/salt/ --log-level=info --retcode-passthrough --force-color state.highstate || exit 1
 
 chmod +x /root/salt/highstate.sh
