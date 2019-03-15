6cfa628169436fceb458a10ac24e07b4ccd6f4eb
Matei Albu <malbu@suse.de>
2019-03-15 14:51:55 +0100

    dont run first_deploymnet_highstate on ubuntu

--- salt/first_deployment_highstate.sh
+++ salt/first_deployment_highstate.sh
@@ -3,6 +3,11 @@
 # Applies the "minimal" state in isolation to set the hostname and update Salt itself
 # then applies the highstate
 
+if [ -f /etc/debian_version ]; then
+   echo "Debian detected. exiting"
+   exit 1;
+fi
+
 salt-call --local --file-root=/root/salt/ --log-level=quiet --output=quiet state.sls default.minimal ||:
 echo "---------------------------------------------------------------------------------------------------"
 salt-call --local --file-root=/root/salt/ --log-level=info --retcode-passthrough --force-color state.highstate || exit 1
